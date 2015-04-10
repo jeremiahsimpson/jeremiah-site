01efb40b2892bc17eeea344ce0936e4c6b0b38df
Jeremiah Simpson <jeremiah@studiobridger.com>
2015-04-10 17:10:36 -0400

    updated css and index of home. added detail page.

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+$(document).on('ready', function() {
+	$('#connect-block-header').on('click', function() {
+		// $('#connect-block').toggleClass('open');
+		$('#connect-block-form').toggle(500);
+	});
+});
